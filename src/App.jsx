0f291c64a70263c8eb48cd81c169b53909180e18
prantikseal <83081442+prantikseal@users.noreply.github.com>
2023-02-14 16:52:06 +0530

    valentines day proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
+import Valentines from "./components/valentines"
 function App() {
 
-
   return (
-    <div className="App ml-3 mt-3">
-      <h1 className="text-xl font-bold ">
-        Hi! My name is <span className="text-blue-500">Prantik!</span>
-      </h1>
-      <p className="text-black-200">Nice to meet ya!</p>
-    </div>
+    <Valentines />
   )
 }
 
